7949fc1870c0e839646998e3d0346141fad674a5
Luke MacKenzie <lmackenzie@reli.sh>
2024-07-09 12:30:46 -0400

    start over

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sentry from '@sentry/astro';
-import spotlightjs from '@spotlightjs/astro';
-
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://lmdev.netlify.app/',
-  integrations: [tailwind(), sentry(), spotlightjs(), preact()]
-});
+export default defineConfig({});
--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const SITE_TITLE = 'Luke MacKenzie.';
-export const SITE_DESCRIPTION =
-  "Hey, I'm Luke MacKenzie, a web developer based in Toronto, Canada.";
